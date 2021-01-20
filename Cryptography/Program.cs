fd216719bd4ca178eca694064ed270e2a658639e
chirchib <dmitry.kurochkin66@gmail.com>
2021-01-20 04:18:11 +0300

    End

--- Cryptography/Program.cs
+++ Cryptography/Program.cs
@@ -7,49 +7,119 @@ namespace Cryptography
     {
         static void Main(string[] args)
         {
-            // двойной квадрат
-            string key11 = "ключ1";
-            string key12 = "ключ2";
-            string text11 = "некоторый текст";
-            Console.WriteLine("Пример работы шифра Двойной квадрат уинстона");
-            Console.WriteLine("Ключ 1 : {0}\nКлюч 2 : {1}", key11, key12);
-            Console.WriteLine("Зашифруем фразу : " + text11);
+			using System;
+			using Cryptography.Ciphers;
 
-            Wheatstone_Double_Square encodecipher1 =
-                new Wheatstone_Double_Square(key11, key12, text11);
+namespace Cryptography
+	{
+		class Program
+		{
+			////////// laba 2
+			//
+			//        static public string text = "DOS";
+			//        static public long p = 23;
+			//
+			//        static void Main(string[] args)
+			//        {
+			//            Console.WriteLine("=====================================================================================================================");
+			//            A_Abonent.PrintLog($"Исходное сообщение: {text}", true);
+			//
+			//            A_Abonent.PrintLog($"Введите Ea, что-бы НОД(Еа, {p - 1}) = 1", true);
+			//            A_Abonent.SetP(p);//Передаем p абоненту А
+			//            A_Abonent.SetE();//Вводим Еа у абонента А
+			//            A_Abonent.SetD();//Формируем D абонента А
+			//
+			//
+			//            B_Abonent.PrintLog($"Введите Eb, что-бы НОД(Еb, {p - 1}) = 1", true);
+			//            B_Abonent.SetP(p);//Передаем p абоненту В
+			//            B_Abonent.SetE();//Вводим Еа у абонента В
+			//            B_Abonent.SetD();//Формируем D абонента В
+			//
+			//
+			//            A_Abonent.PrintE();//Выводим закрытый ключ абонента А
+			//            B_Abonent.PrintE();//Выводим закрытый ключ абонента В
+			//
+			//
+			//            A_Abonent.TextToNumberEncrypt(text);//Текст в числовом представлении
+			//            A_Abonent.PrintNumberEncrypt();//Вывод текста  в числ. представлении  
+			//
+			//
+			//            long[] C1 = A_Abonent.FirstStepE(A_Abonent.GetNumberEncrypt());////////////////////////////////////////////////////////////////////////
+			//            //C1 = new long[] { 1, 2, 3, 5 };
+			//            A_Abonent.PrintLog("Текст в числовом представлении C1: " + Shamir.NumberEncryptToString(C1), true);
+			//            A_Abonent.PrintLog("Текст в символьном представлении С1: " + Shamir.NumberEncryptToTextWithSpace(C1), true);
+			//
+			//            long[] C2 = B_Abonent.FirstStepE(C1);
+			//            B_Abonent.PrintLog("Текст в числовом представлении C2: " + Shamir.NumberEncryptToString(C2), true);
+			//            B_Abonent.PrintLog("Текст в символьном представлении С2: " + Shamir.NumberEncryptToTextWithSpace(C2), true);
+			//
+			//            long[] C3 = A_Abonent.SecondStepD(C2);
+			//            A_Abonent.PrintLog("Текст в числовом представлении C3: " + Shamir.NumberEncryptToString(C3), true);
+			//            A_Abonent.PrintLog("Текст в символьном представлении С3: " + Shamir.NumberEncryptToTextWithSpace(C3), true);
+			//
+			//            long[] C4 = B_Abonent.SecondStepD(C3);
+			//            B_Abonent.PrintLog("Текст в числовом представлении C4: " + Shamir.NumberEncryptToString(C4), true);
+			//            B_Abonent.PrintLog("Текст в символьном представлении С4: " + Shamir.NumberEncryptToTextWithSpace(C4), true);
+			//
+			//            B_Abonent.PrintLog($"Полученное сообщение: {Shamir.NumberEncryptToText(C4)}", true);
+			//            Console.WriteLine("=====================================================================================================================");
+			//
+			//        }
+			//
+			//
+			///////////////// laba 3
+			///
+			//        static void Main(string[] args)
+			//        {
+			//            RSA algorithmRSA = new RSA(17, 36563, 57731);//Генерируем ключи
+			//
+			//            algorithmRSA.getPublicKey(out long e, out long n1);//Возврат открытых ключей
+			//
+			//            string[] rsaEncrypt = algorithmRSA.Encrypt("PUBLIC", e, n1);//Зашифровываем сообщение
+			//
+			//            algorithmRSA.getPrivateKey(out long d, out long n2);//Возврат открытых ключей
+			//
+			//            string rsaDecrypt = algorithmRSA.Decrypt(rsaEncrypt, d, n2);//Дешифровываем сообщение
+			//
+			//
+			//            algorithmRSA.CipherLog();
+			//        }
+			//
+			//        
+			/////////// laba 4 
+			///
+			//        static public string text = "CTC";
+			//        static public int p = 42467;
+			//        static public long[] NumberEncrypt;
+			//        
+			//        static void Main(string[] args)
+			//        {
+			//            Console.WriteLine("=====================================================================================================================");
+			//            Abonent1.PrintLog($"ИСходное сообщение: {text}", true);
+			//        
+			//            //Этап 1
+			//            Abonent1.SetP(p);//Абонент А выбирает Р
+			//            Abonent1.EnterG();//Абонент А вводит G
+			//            Abonent1.EnterX();//Абонент A вводит Х
+			//            Abonent1.CalculateY();//Высчитываем Y
+			//            Abonent1.PrintOpenKeys();//Вывод открытых ключей
+			//            Abonent1.PrintPrivateKeys();//Вывод закрытых ключей
+			//            Abonent1.SendOpenKeys();//Отправляем абоненту B открытый ключ
+			//            //Этап 2
+			//            NumberEncrypt = ELGAmal.TextToNumberEncrypt(text);
+			//            ELGAmal.PrintNumberEncrypt(NumberEncrypt, true);
+			//        
+			//            //Этап 3
+			//            Abonent1.SignatureGeneration();//Генерация ЭЦП
+			//        
+			//            //Этап 4
+			//            Abonent2.verificationOfDigitalSignature(Abonent1.a, Abonent1.b, NumberEncrypt);
+			//            Console.WriteLine("=====================================================================================================================");
+			//        }
+			//    
+		}
+	}
 
-            Console.WriteLine("Зашифрованная фраза : " + encodecipher1.Encoded());
 
-            string text12 = "6oчFЙtFQ!.r!БмrФ";
-            Console.WriteLine("Расшифруем фразу : " + text12);
-
-            Wheatstone_Double_Square decodecipher1 =
-               new Wheatstone_Double_Square(key11, key12, text12);
-
-            Console.WriteLine("Расшифрованная фраза : " + decodecipher1.Decoded());
-
-            // двойная перестановка
-            string key21 = "СКАНЕР";
-            string key22 = "4123";
-            string text21 = "системный пароль изменен";
-            Console.WriteLine("\nПример работы шифра двойной перестановки");
-            Console.WriteLine("Ключ 1 : {0}\nКлюч 2 : {1}", key21, key22);
-            Console.WriteLine("Зашифруем фразу : {0}", text21);
-
-            
-            Double_permutation_of_characters encodecipher2 =
-                new Double_permutation_of_characters(key21, key22, text21);
-            
-
-            Console.WriteLine("Зашифрованная фраза : " + encodecipher2.Encoded());
-
-            string text22 = "ЙЛЕСП ЕЕЫОМИ ЬНТАИНМНРЗС";
-            Console.WriteLine("Расшифруем фразу : " + text22);
-
-            Double_permutation_of_characters decodecipher2 =
-                new Double_permutation_of_characters(key21, key22, text22);
-
-            Console.WriteLine("Расшифрованная фраза : " + decodecipher2.Decoded());
-        }
-    }
+}
 }
